7c9cf696185f3f862b4685e1178d5d45904d9833
Harrison <Harrisontbusby@gmail.com>
2023-05-01 10:40:43 -0700

    Committ

---
+++ Controllers/UserController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodewarsSprintBackend;
+
+
+namespace CodewarsSprintBackend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+  
+}
